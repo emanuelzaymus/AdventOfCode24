7ddae318ec4724332c8c2a0026844bcd0071ce47
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2024-12-02 16:46:44 +0100

    Day 2 - task 1 refactor

--- AdventOfCode24/Day02/ReportsLevelAnalysis.cs
+++ AdventOfCode24/Day02/ReportsLevelAnalysis.cs
@@ -11,7 +11,7 @@ public static class ReportsLevelAnalysis
     {
         var numberOfSafeReports = NumberOfSafeReports(Input);
 
-        Console.WriteLine(numberOfSafeReports);
+        Console.WriteLine(numberOfSafeReports); // 639
     }
 
     public static int NumberOfSafeReports(string input) =>
@@ -29,7 +29,13 @@ public static class ReportsLevelAnalysis
         return IsGraduallyIncreasing(levels) || IsGraduallyDecreasing(levels);
     }
 
-    private static bool IsGraduallyIncreasing(List<int> levels)
+    private static bool IsGraduallyIncreasing(List<int> levels) =>
+        AllHaveGradualOffset(levels, (first, second) => second - first);
+
+    private static bool IsGraduallyDecreasing(List<int> levels) =>
+        AllHaveGradualOffset(levels, (first, second) => first - second);
+
+    private static bool AllHaveGradualOffset(List<int> levels, Func<int, int, int> calculateOffset)
     {
         var first = levels.Take(..^1);
         var second = levels.Take(1..);
@@ -37,20 +43,7 @@ public static class ReportsLevelAnalysis
         return first.Zip(second)
             .All(pair =>
             {
-                var offset = pair.Second - pair.First;
-                return offset is >= 1 and <= 3;
-            });
-    }
-
-    private static bool IsGraduallyDecreasing(List<int> levels)
-    {
-        var first = levels.Take(..^1);
-        var second = levels.Take(1..);
-
-        return first.Zip(second)
-            .All(pair =>
-            {
-                var offset = pair.First - pair.Second;
+                var offset = calculateOffset(pair.First, pair.Second);
                 return offset is >= 1 and <= 3;
             });
     }
--- AdventOfCode24/Program.cs
+++ AdventOfCode24/Program.cs
@@ -1,8 +1,1 @@
-﻿// Day 1
-
-// AdventOfCode24.Day01.LocationIds.RunTask1();
-// AdventOfCode24.Day01.LocationIds.RunTask2();
-
-// Day 2
-
-AdventOfCode24.Day02.ReportsLevelAnalysis.RunTask1();
+﻿Console.WriteLine("Hello AdventOfCode24!");

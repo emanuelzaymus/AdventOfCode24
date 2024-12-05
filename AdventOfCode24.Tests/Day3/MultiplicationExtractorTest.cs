e80bbb85d0d9e966b6f539846a7f8b3aaba46494
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2024-12-05 10:07:24 +0100

    Day 3 - task 2

--- AdventOfCode24.Tests/Day3/MultiplicationExtractorTest.cs
+++ AdventOfCode24.Tests/Day3/MultiplicationExtractorTest.cs
@@ -10,11 +10,21 @@ public class MultiplicationExtractorTest
 {
     private const string ExampleInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
+    private const string ExampleInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
     [TestMethod]
     public void MultiplicationSum_ExampleInput_ShouldReturnCorrectResult()
     {
         var multiplicationSum = MultiplicationExtractor.MultiplicationSum(ExampleInput);
 
         Assert.AreEqual(161, multiplicationSum);
+    }
+
+    [TestMethod]
+    public void MultiplicationSumWithDisabling_ExampleInput_ShouldReturnCorrectResult()
+    {
+        var multiplicationSumWithDisabling = MultiplicationExtractor.MultiplicationSumWithDisabling(ExampleInput2);
+
+        Assert.AreEqual(48, multiplicationSumWithDisabling);
     }
 }
--- AdventOfCode24/Day3/MultiplicationExtractor.cs
+++ AdventOfCode24/Day3/MultiplicationExtractor.cs
@@ -46,4 +46,120 @@ public static class MultiplicationExtractor
             .Select(pair => (long)pair.Item1 * pair.Item2)
             .Sum();
     }
+
+    public static void RunTask2()
+    {
+        var multiplicationSumWithDisabling = MultiplicationSumWithDisabling(Input);
+
+        Console.WriteLine(multiplicationSumWithDisabling); // 72948684
+    }
+
+    public static long MultiplicationSumWithDisabling(string input)
+    {
+        var multiplicationInstructions = new List<(bool enbled, int firstNumber, int secondNumber)>();
+
+        var instructionsEnabled = true;
+        var startIndex = 0;
+
+        (Instruction instruction, int nextInstructionStartIndex)? nextInstruction;
+        while ((nextInstruction = FindNextInstruction(input, startIndex)) != null)
+        {
+            startIndex = nextInstruction.Value.nextInstructionStartIndex;
+
+            switch (nextInstruction.Value.instruction)
+            {
+                case Instruction.Multiplication:
+                {
+                    var multiplicationNumbers = ExtractMultiplicationNumbers(input, startIndex);
+
+                    if (multiplicationNumbers != null)
+                    {
+                        var (firstNumber, secondNumber) = multiplicationNumbers.Value;
+                        multiplicationInstructions.Add((instructionsEnabled, firstNumber, secondNumber));
+                    }
+
+                    break;
+                }
+                case Instruction.Do:
+                {
+                    instructionsEnabled = true;
+                    break;
+                }
+                case Instruction.Dont:
+                {
+                    instructionsEnabled = false;
+                    break;
+                }
+                default:
+                {
+                    throw new Exception("Unknown instruction");
+                }
+            }
+        }
+
+        return multiplicationInstructions
+            .Where(pair => pair.enbled)
+            .Select(pair => (long)pair.firstNumber * pair.secondNumber)
+            .Sum();
+    }
+
+    private static (Instruction instruction, int nextInstructionStartIndex)? FindNextInstruction(string input,
+        int startIndex)
+    {
+        var multiplicationIndex = input.IndexOf("mul(", startIndex, StringComparison.Ordinal);
+        multiplicationIndex = multiplicationIndex != -1 ? multiplicationIndex : int.MaxValue;
+
+        var doIndex = input.IndexOf("do()", startIndex, StringComparison.Ordinal);
+        doIndex = doIndex != -1 ? doIndex : int.MaxValue;
+
+        var dontIndex = input.IndexOf("don't()", startIndex, StringComparison.Ordinal);
+        dontIndex = dontIndex != -1 ? dontIndex : int.MaxValue;
+
+        if (multiplicationIndex < doIndex && multiplicationIndex < dontIndex)
+        {
+            return (Instruction.Multiplication, multiplicationIndex + 4);
+        }
+
+        if (doIndex < multiplicationIndex && doIndex < dontIndex)
+        {
+            return (Instruction.Do, doIndex + 4);
+        }
+
+        if (dontIndex < multiplicationIndex && dontIndex < doIndex)
+        {
+            return (Instruction.Dont, dontIndex + 7);
+        }
+
+        return null;
+    }
+
+    private static (int firstNumber, int secondNumber)? ExtractMultiplicationNumbers(string input,
+        int firstNumberStartIndex)
+    {
+        var commaIndex = input.IndexOf(',', firstNumberStartIndex);
+
+        var firstNumberStr = input.AsSpan(firstNumberStartIndex, commaIndex - firstNumberStartIndex);
+        if (!int.TryParse(firstNumberStr, out var firstNumber))
+        {
+            return null;
+        }
+
+        var secondNumberStartIndex = commaIndex + 1;
+        var endParenthesesIndex = input.IndexOf(')', secondNumberStartIndex);
+
+        var secondNumberStr = input.AsSpan(secondNumberStartIndex, endParenthesesIndex - secondNumberStartIndex);
+        if (!int.TryParse(secondNumberStr, out var secondNumber))
+        {
+            return null;
+        }
+
+        return (firstNumber, secondNumber);
+    }
+
+    private enum Instruction
+    {
+        Multiplication,
+        Do,
+        Dont
+    }
 }

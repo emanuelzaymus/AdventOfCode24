b9b323ec83e9b605c8a11706f7b7e0ea477a5e59
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2024-12-11 16:28:56 +0100

    Removed semicolon

--- AdventOfCode24/Day06/Position.cs
+++ AdventOfCode24/Day06/Position.cs
@@ -5,4 +5,4 @@ internal readonly record struct Position(int RowIndex, int ColumnIndex)
     public Position Move(Direction direction) => new(
         RowIndex + direction.RowOffset,
         ColumnIndex + direction.ColumnOffset);
-};
+}

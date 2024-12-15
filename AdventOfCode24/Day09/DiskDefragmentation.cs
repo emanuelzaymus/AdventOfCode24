6921ef0c687d80dbc2f0bc0b6c67baa24ba10405
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2024-12-15 12:56:31 +0100

    Day 9 - task 1 refactor

--- AdventOfCode24/Day09/DiskDefragmentation.cs
+++ AdventOfCode24/Day09/DiskDefragmentation.cs
@@ -5,6 +5,8 @@ namespace AdventOfCode24.Day09;
 /// </summary>
 public static class DiskDefragmentation
 {
+    private const int InvalidBlockId = -1;
+
     private static string Input => File.ReadAllText("Data/day09.txt");
 
     public static void RunTask1()
@@ -25,59 +27,61 @@ public static class DiskDefragmentation
 
     private static List<int> CreateDisk(string diskMap)
     {
-        var result = new List<int>();
+        var diskBlockIds = new List<int>();
 
         var isBlock = true;
-        var currentId = -1;
-        foreach (var currentBlockLength in diskMap.Select(ConvertCharToInt))
+        var currentBlockId = -1;
+        foreach (var currentBlockCount in diskMap.Select(ConvertCharToInt))
         {
-            var idToWrite = -1;
+            var blockIdToWrite = InvalidBlockId;
             if (isBlock)
             {
-                idToWrite = ++currentId;
+                blockIdToWrite = ++currentBlockId;
             }
 
-            for (var j = 0; j < currentBlockLength; j++)
+            for (var j = 0; j < currentBlockCount; j++)
             {
-                result.Add(idToWrite);
+                diskBlockIds.Add(blockIdToWrite);
             }
 
             isBlock = !isBlock;
         }
 
-        return result;
+        return diskBlockIds;
     }
 
     private static void OptimizeDisk(List<int> disk)
     {
-        var positionToWriteIndex = 0;
+        var invalidBlockIndex = 0;
         for (var i = disk.Count - 1; i >= 0; i--)
         {
-            var lastDigit = disk[i];
-            if (lastDigit == -1) continue;
+            var currentBlockId = disk[i];
+            if (!IsValidDiskBlock(currentBlockId)) continue;
 
-            while (positionToWriteIndex < i && disk[positionToWriteIndex] != -1)
+            while (invalidBlockIndex < i && IsValidDiskBlock(disk[invalidBlockIndex]))
             {
-                positionToWriteIndex++;
+                invalidBlockIndex++;
             }
 
-            if (positionToWriteIndex >= i)
+            if (invalidBlockIndex >= i)
             {
                 break;
             }
 
-            disk[positionToWriteIndex] = lastDigit;
-            disk[i] = -2;
+            disk[invalidBlockIndex] = currentBlockId;
+            disk[i] = InvalidBlockId;
         }
     }
 
     private static long CalculateDiskChecksum(List<int> disk)
     {
         return disk
-            .Where(value => value >= 0)
-            .Select((value, index) => value * (long)index)
+            .Where(IsValidDiskBlock)
+            .Select((blockId, index) => blockId * (long)index)
             .Sum();
     }
+
+    private static bool IsValidDiskBlock(int blockId) => blockId != InvalidBlockId;
 
     private static int ConvertCharToInt(char digit) => digit - '0';
 }
